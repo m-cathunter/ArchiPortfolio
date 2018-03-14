b658c6863d953b127ed19afe85628c00c0d60925
m-cathunter <m.kotolowska@gmail.com>
2018-03-14 11:41:14 +0100

    create new pages: index.html.erb and _form.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,18 @@
 class ProjectsController < ApplicationController
   def index
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+  end
+
+  private
+      def project_params
+      params.require(:project).permit(:title, :description, :author)
+      end 
+
 end

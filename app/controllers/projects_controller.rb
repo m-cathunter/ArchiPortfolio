90b2f223159f35ed0fcfbc857b0f0129477cff71
m-cathunter <m.kotolowska@gmail.com>
2018-04-02 23:43:41 +0200

    Create Navbar new option - 'Add Project'

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -38,7 +38,7 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    #@project = Project.find(params[:id])
+    @project = Project.find(params[:id])
     @project.destroy
 
     redirect_to project_path

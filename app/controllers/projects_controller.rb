331c335af904496284acf0669c755741948642b1
m-cathunter <m.kotolowska@gmail.com>
2018-04-03 19:17:35 +0200

    Make current user's conditions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,8 +17,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_params)
-    @project.user=current_user
+    @project = current_user.projects.build(project_params)
 
     if @project.save
       redirect_to @project

f11f5a20460712772183be9096d16743c7e5e31c
m-cathunter <m.kotolowska@gmail.com>
2018-04-09 11:56:00 +0200

    Make changes on images in index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .

48ef3ba7c5748a3d5d20529595a539d7f5188f54
m-cathunter <m.kotolowska@gmail.com>
2018-04-11 10:50:17 +0200

    Enter changes in rewiews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,21 @@
 class ReviewsController < ApplicationController
 
+  before_action :find_project
+
   def new
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+    @review.project_id = @project.id
+    @review.user_id = current_user.id
+  end
+
+  if @review.save
+    redirect_to project_path(@project)
+  else
+    render 'new'
   end
 
   private
@@ -14,4 +24,7 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:rating, :comment)
   end
 
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
 end

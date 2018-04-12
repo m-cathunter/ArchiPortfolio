daa309afa56e2f99be44142b0a356e11912a25aa
m-cathunter <m.kotolowska@gmail.com>
2018-04-12 11:25:26 +0200

    Create edit and update for reviews in reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,15 @@
 class ReviewsController < ApplicationController
   before_action :find_project
+  before_action :find_review, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
   end
 
   def create
-      @review = Review.new(review_params)
-      @review.project_id = @project.id
-      @review.user_id = current_user.id
+    @review = Review.new(review_params)
+    @review.project_id = @project.id
+    @review.user_id = current_user.id
 
     if @review.save
       redirect_to project_path(@project)
@@ -16,6 +17,19 @@ class ReviewsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+
+  end
+
+  def update
+
+    if @review.update(review_params)
+      redirect_to project_path(@project)
+    else
+      render 'edit'
+  end
+
   private
 
   def review_params
@@ -25,4 +39,9 @@ class ReviewsController < ApplicationController
   def find_project
     @project = Project.find(params[:project_id])
   end
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
 end

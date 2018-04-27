ff1aeba5b73bc77d12405f9574db4a7b839aa1c5
m-cathunter <m.kotolowska@gmail.com>
2018-04-27 16:50:05 +0200

    Change authentication options for unlogged user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     if params[:category].blank?
       @projects = Project.all

b58b894da2e968c7d6c79a142bb5a89222b374c4
m-cathunter <m.kotolowska@gmail.com>
2018-04-28 13:55:26 +0200

    Change look of pictures

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,6 @@ class Project < ApplicationRecord
 #  validates :title, presence: true,
       #      length: { minimum: 6}
 
-  has_attached_file :project_img, :styles => { :project_index => "250x250", :project_show => "250x250" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :project_img, :styles => { :project_index => "350x350", :project_show => "1600x1600" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :project_img, content_type: /\Aimage\/.*\z/
 end
